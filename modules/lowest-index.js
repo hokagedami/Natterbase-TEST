04d56cf1cd9c868e2ccbc6a2fc99121e41b58932
Michael Akinyemi <mikedamilare@gmail.com>
2019-10-07 17:28:54 +0100

    Adding Procfile for Heroku hosting

--- modules/lowest-index.js
+++ modules/lowest-index.js
@@ -6,12 +6,12 @@ module.exports = lowestIndex = (magicSources, distances) => {
     const distancesSum = distances.reduce((a, b) => { return a + b}, 0);
     const sourcesSum = distances.reduce((a, b) => { return a + b}, 0);
     if (distancesSum > sourcesSum) return -1;
-    let distanceCanCover = 0;
+    let distanceCanCover = 0, failedIndex = [], passedIndex = [];
     for (let i = 0; i < magicSources.length; i++) {
         distanceCanCover += magicSources[i];
         distanceCanCover -= distances[i];
         if (distanceCanCover < 0) {
-            
+            failedIndex.push(i)
         }
     }
 };
